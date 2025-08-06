0bf84d9a94f28dd4cd672ae23a574609f7d6e4b9
Yvonnick FRIN <frin.yvonnick@gmail.com>
2025-08-06 16:11:06 +0200

    feat(api): add decrypt redirectionHash logic to getModule usecase

--- api/src/devcomp/domain/usecases/get-module.js
+++ api/src/devcomp/domain/usecases/get-module.js
@@ -1,5 +1,18 @@
-async function getModule({ slug, moduleRepository }) {
-  return moduleRepository.getBySlug({ slug });
+import { config } from '../../../shared/config.js';
+import { cryptoService } from '../../../shared/domain/services/crypto-service.js';
+
+async function getModule({ slug, redirectionHash, moduleRepository }) {
+  const module = await moduleRepository.getBySlug({ slug });
+
+  if (redirectionHash) {
+    const redirectionUrl = await cryptoService.decrypt(redirectionHash, config.module.secret);
+
+    if (redirectionUrl) {
+      module.setRedirectionUrl(redirectionUrl);
+    }
+  }
+
+  return module;
 }
 
 export { getModule };
